456e1a61d81d5b3401ec23cca36ff5a76c756141
Mateus Luiz <mateuslfreitasb@gmail.com>
2021-09-08 21:51:07 -0300

    Add boilerplate code

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+def sub_bytes(arg):
+    pass
+
+def shift_rows(arg):
+    pass
+
+def mix_columns(arg):
+    pass
+
+def add_key(arg):
+    pass
+
+class AES(object):
+    def __init__(self, key):
+
+    def encrypt(self, plaintext)
