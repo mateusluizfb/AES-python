330d53464637f2d53edbba6d19eca3e99e7a714e
Mateus Luiz <mateuslfreitasb@gmail.com>
2021-09-09 19:22:13 -0300

    Add sub-bytes and shift rows

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import numpy as np
 
+# Rows should handled as columns, and columns as rows
+def convert_to_matrix(plaintext):
+    if len(plaintext) > 16:
+        return None
+
+    aes_matrix = []
+
+    for i in range(0, len(plaintext), 4):
+        four_bytes_section = plaintext[i: i + 4]
+        caracter_list = list(four_bytes_section)
+        aes_matrix.append(caracter_list)
+
+    return aes_matrix
 
 def sub_bytes(arg):
-    pass
+    return np.transpose(arg)
 
 def shift_rows(arg):
-    pass
+    print(arg)
+
+    arg[0] = np.roll(arg[0], 0)
+    arg[1] = np.roll(arg[1], 1)
+    arg[2] = np.roll(arg[2], 2)
+    arg[3] = np.roll(arg[3], 3)
+
+    return arg
 
 def mix_columns(arg):
     pass
@@ -12,7 +33,12 @@ def mix_columns(arg):
 def add_key(arg):
     pass
 
-class AES(object):
-    def __init__(self, key):
+def encrypt(plaintext):
+    plaintext_matrix = convert_to_matrix(plaintext)
+    result = None
+    result = sub_bytes(plaintext_matrix)
+    result = shift_rows(result)
 
-    def encrypt(self, plaintext)
+    print(result)
+
+print(encrypt("1234567812345678"))
