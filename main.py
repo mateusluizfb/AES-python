3d63bf66c549d86fa96216fc48c8a650221603b2
Mateus Luiz <mateuslfreitasb@gmail.com>
2021-09-09 21:34:38 -0300

    Remove wrong mix_columns implementation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
+import codecs
 
-# Rows should handled as columns, and columns as rows
 def convert_to_matrix(plaintext):
     if len(plaintext) > 16:
         return None
@@ -9,13 +9,12 @@ def convert_to_matrix(plaintext):
 
     for i in range(0, len(plaintext), 4):
         four_bytes_section = plaintext[i: i + 4]
-        bytes_to_int_list = list(four_bytes_section)
-        aes_matrix.append(bytes_to_int_list)
+        aes_matrix.append(list(four_bytes_section))
 
     return aes_matrix
 
 def sub_bytes(arg):
-    return np.transpose(arg)
+    return np.transpose(convert_to_matrix(arg))
 
 def shift_rows(arg):
     arg[0] = np.roll(arg[0], 0)
@@ -25,33 +24,40 @@ def shift_rows(arg):
 
     return arg
 
-def mix_columns(arg):
-    mix_column_matrix = np.array([
+def mix_columns(matrix):
+    # https://www.angelfire.com/biz7/atleast/mix_columns.pdf
+
+    mix_columns_base_matrix = [
         [2 , 3, 1, 1],
         [1 , 2, 3, 1],
         [1 , 1, 2, 3],
         [3 , 1, 1, 2]
-    ])
+    ]
 
-    for i in range(4):
-        res = np.matmul(mix_column_matrix, np.array(arg[i]))
-        arg[i] = res
+    # We need to multiply the mix_columns_base_matrix by each matrix's column.
+    # Be aware that the results for each multiplication MUST only have byte results, in other words, must be between:
+    # decimal: 0 and 255 | hex: 00 and FF | binary: 00000000 and 11111111
 
-    return arg
+    return matrix # array de arrays de bytes
 
-def add_key(arg):
-    pass
-
-def encrypt(plaintext):
-    print(plaintext)
-    plaintext_matrix = convert_to_matrix(plaintext)
+def encrypt(key, plaintext):
+    
     result = None
-    result = sub_bytes(plaintext_matrix)
+    result = sub_bytes(plaintext)
     result = shift_rows(result)
     result = mix_columns(result)
     return result
 
+# We will receive chunks of 16 bytes to encrypt when reading the file:
+#
+# f = open("selfie.jpeg", "rb")
+# try:
+#     byte = f.read(1)
+#     while byte != "":
+#         # Do stuff with byte.
+#         byte = f.read(1)
+#         print(byte)
+# finally:
+#     f.close()
 
-# print(bytes([56]))
-print(encrypt(b'1234567812345678'))
-# print("1234567812345678".encode())
+print(encrypt('key', [b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8',b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8']))
